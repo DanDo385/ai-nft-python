1b7829ebfcb6e0eea8c469483357cfd3edfc0e35
Daniel Magro <dando385@yahoo.com>
2024-05-14 21:45:10 -0400

    added deployment script

---
+++ backend/scripts/deploy.py
@@ -0,0 +1,36 @@
+from web3 import Web3
+import json
+import os
+
+# Connect to local Ethereum node
+ganache_url = 'http://127.0.0.1:8545'
+web3 = Web3(Web3.HTTPProvider(ganache_url))
+
+# Check if connected to the node
+if not web3.isConnected():
+    raise Exception("Failed to connect to Ethereum node")
+
+# Set default account
+web3.eth.default_account = web3.eth.accounts[0]
+
+# Load ABI and bytecode
+artifacts_dir = os.path.join('backend', 'artifacts')
+with open(os.path.join(artifacts_dir, 'MyContract.abi'), 'r') as abi_file:
+    abi = json.load(abi_file)
+
+with open(os.path.join(artifacts_dir, 'MyContract.bytecode'), 'r') as bytecode_file:
+    bytecode = bytecode_file.read()
+
+# Deploy contract
+MyContract = web3.eth.contract(abi=abi, bytecode=bytecode)
+tx_hash = MyContract.constructor().transact()
+tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
+
+# Save contract address
+contract_address = tx_receipt.contractAddress
+deployment_info = {'contract_address': contract_address}
+with open(os.path.join(artifacts_dir, 'deployed_contract_address.json'), 'w') as address_file:
+    json.dump(deployment_info, address_file, indent=4)
+
+print(f"Contract deployed at address: {contract_address}")
+print("Deployment information saved in the artifacts directory.")
